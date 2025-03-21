311626778cfb7bc71986d3602eb6f744b7ad4f67
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-03-21 10:38:28 -0400

    Connected Phaser, centered canvas

---
+++ main.js
@@ -0,0 +1,43 @@
+const config = {
+   type: Phaser.AUTO, // Which renderer to use
+   width: 800, // Canvas width in pixels
+   height: 600, // Canvas height in pixels
+   parent: "game-container", // ID of the DOM element to add the canvas to
+   scene: {
+      preload: preload,
+      create: create,
+      update: update
+   }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+   this.load.image("mario-tiles", "../assets/mario-tileset.webp");
+}
+
+function create() {
+   // Load a map from a 2D array of tile indices
+   const level = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 2, 3, 0, 0, 0, 1, 2, 3, 0],
+      [0, 5, 6, 7, 0, 0, 0, 5, 6, 7, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 14, 13, 14, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 14, 14, 14, 14, 14, 0, 0, 0, 15],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 15],
+      [35, 36, 37, 0, 0, 0, 0, 0, 15, 15, 15],
+      [39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39]
+   ];
+
+   // When loading from an array, make sure to specify the tileWidth and tileHeight
+   const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
+   const tiles = map.addTilesetImage("mario-tiles");
+   const layer = map.createLayer(0, tiles, 0, 0);
+}
+
+function update(time, delta) {
+   // Runs once per frame for the duration of the scene
+}
